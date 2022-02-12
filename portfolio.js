7a9ae4c784894704c8bc4272e71c5d32f2b5eddc
Pacific-Kai <mechaoishiiosaka1212nz@gmail.com>
2022-02-13 06:31:01 +0900

    commit init

---
+++ portfolio.js
@@ -0,0 +1,32 @@
+function TextRandomAnimeControl() {
+	$('.portfolio').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass("appearRandomtext");
+
+		} else {
+			$(this).removeClass("appearRandomtext");
+		}
+	});
+}
+
+$(window).scroll(function () {
+	TextRandomAnimeControl();
+});
+
+
+$(window).on('load', function () {
+	var element = $(".portfolio");
+	element.each(function () {
+		var text = $(this).text();
+		var textbox = '';
+		text.split('').forEach(function (t) {
+			textbox += '<span>' + t + '</span>';
+		});
+		$(this).html(textbox);
+	});
+
+	TextRandomAnimeControl();
+});
