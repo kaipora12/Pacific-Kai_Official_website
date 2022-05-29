6e42e9f55bbf44ced4f34c06aba586af11794eb8
Pacific-Kai <mechaoishiiosaka1212nz@gmail.com>
2022-05-30 05:11:49 +0900

    commit init

--- illustrator/illustrator.js
+++ illustrator/illustrator.js
@@ -1,11 +1,33 @@
-$(".openbtn").click(function () {
-	$(this).toggleClass('active');
-    $("#g-nav").toggleClass('panelactive');
-    $("#container").toggleClass('mainblur');
+function TextRandomAnimeControl() {
+	$('.h5').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass("appearRandomtext");
+
+		} else {
+			$(this).removeClass("appearRandomtext");
+		}
+	});
+}
+
+$(window).scroll(function () {
+	TextRandomAnimeControl();
 });
 
-$("#g-nav a").click(function () {
-    $(".openbtn").removeClass('active');
-    $("#g-nav").removeClass('panelactive');
-    $("#container").removeClass('mainblur');
+
+$(window).on('load', function () {
+	var element = $(".h5");
+	element.each(function () {
+		var text = $(this).text();
+		var textbox = '';
+		text.split('').forEach(function (t) {
+			textbox += '<span>' + t + '</span>';
+		});
+		$(this).html(textbox);
+	});
+
+	TextRandomAnimeControl();
 });
+
